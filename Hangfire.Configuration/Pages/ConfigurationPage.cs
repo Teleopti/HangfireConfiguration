5ec29a662a9b64f99b025209a9193b4e9a9ebf28
Alina_Per_Mathias <Alina_Per_Mathias_LevelUp@teleopti.com>
2019-06-27 16:11:24 +0200

    add description

--- Hangfire.Configuration/Pages/ConfigurationPage.cs
+++ Hangfire.Configuration/Pages/ConfigurationPage.cs
@@ -24,10 +24,21 @@ namespace Hangfire.Configuration.Pages
 		{
 			WriteLiteral("\r\n");
 			WriteLiteral("<h2>Hangfire worker configuration</h2>");
+			WriteLiteral("<p>");
+			WriteLiteral("Use this configuration to set the goal number of workers to dynamically scale workers per server.");
+			WriteLiteral("<br>");
+			WriteLiteral("On start up of each Hangfire server, the server will be assigned a number of workers approximate for equal distribution of the goal workers count.");
+			WriteLiteral("<br>");
+			WriteLiteral("This is an approximation for reasons like: the number of existing servers is not exact, rounding, minimum 1 worker assigned.");
+			WriteLiteral("<br>");
+			WriteLiteral("As the servers randomly reset, the goal will eventually be met.");
+			WriteLiteral("<br>");
+			WriteLiteral("Default goal is 10.");
+			WriteLiteral("</p>");
 			WriteLiteral("\r\n");
 			WriteLiteral("\r\n");
 			WriteLiteral($@"<form  action=""{_basePath}/saveConfig"">
-							<label for=""workers"">Workers: </label>
+							<label for=""workers"">Worker Goal Count: </label>
 							<input type=""number"" value=""{getWorkers()}"" id =""workers"" name=""workers"" style='width:60px'>
 							<button type=""submit"">Submit</button>");
 			if (_displayConfirmationMessage)
