73b6756eb3ec91babfbdaf19cb3fd21530652a5b
Alina_Per_Mathias <Alina_Per_Mathias_LevelUp@teleopti.com>
2019-08-13 09:21:00 +0200

    create hangfire schema infra

--- Hangfire.Configuration.Test/Infrastructure/SchemaCreatorTest.cs
+++ Hangfire.Configuration.Test/Infrastructure/SchemaCreatorTest.cs
@@ -1,5 +1,6 @@
+using System;
 using System.Data.SqlClient;
-using System.Linq;
+using Dapper;
 using Xunit;
 
 namespace Hangfire.Configuration.Test.Infrastructure
@@ -11,6 +12,7 @@ namespace Hangfire.Configuration.Test.Infrastructure
         public void ShouldConnect()
         {
             var creator = new HangfireSchemaCreator();
+            
             creator.TryConnect(ConnectionUtils.GetConnectionString());
         }
 
@@ -18,7 +20,29 @@ namespace Hangfire.Configuration.Test.Infrastructure
         public void ShouldThrowSqlExceptionWhenNoDatabase()
         {
             var creator = new HangfireSchemaCreator();
+            
             Assert.ThrowsAny<SqlException>(() => creator.TryConnect(@"Server=.\;Database=DoesNotExist;Trusted_Connection=True;"));
+        }
+        
+        [Fact, CleanDatabase]
+        public void ShouldCreateSchema()
+        {
+            var creator = new HangfireSchemaCreator();
+            
+            creator.CreateHangfireSchema("HangfireTestSchema", ConnectionUtils.GetConnectionString());
+
+            using (var conn = new SqlConnection(ConnectionUtils.GetConnectionString()))
+            {
+                Assert.Equal("HangfireTestSchema", conn.ExecuteScalar<string>("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'HangfireTestSchema'"));
+            }
+        }
+        
+        [Fact, CleanDatabase]
+        public void ShouldThrowOnCreateWhenInvalidConnectionString()
+        {
+            var creator = new HangfireSchemaCreator();
+            
+            Assert.ThrowsAny<Exception>(() => creator.CreateHangfireSchema("HangfireTestSchema", "InvalidConnectionString"));
         }
     }
 }
--- Hangfire.Configuration/HangfireSchemaCreator.cs
+++ Hangfire.Configuration/HangfireSchemaCreator.cs
@@ -1,13 +1,23 @@
+using System.Data.SqlClient;
+
 namespace Hangfire.Configuration
 {
     public class HangfireSchemaCreator : IHangfireSchemaCreator
     {
         public void TryConnect(string connectionString)
         {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+            }
         }
 
         public void CreateHangfireSchema(string schemaName, string connectionString)
         {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                Hangfire.SqlServer.SqlServerObjectsInstaller.Install(conn, schemaName, true);
+            }
         }
     }
 }
