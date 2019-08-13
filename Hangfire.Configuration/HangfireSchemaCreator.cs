73877a936512be79f5cf46fd43678c108d72a68f
Alina_Per_Mathias <Alina_Per_Mathias_LevelUp@teleopti.com>
2019-08-13 09:46:56 +0200

    minor cleanup

--- Hangfire.Configuration/HangfireSchemaCreator.cs
+++ Hangfire.Configuration/HangfireSchemaCreator.cs
@@ -7,17 +7,13 @@ namespace Hangfire.Configuration
         public void TryConnect(string connectionString)
         {
             using (var conn = new SqlConnection(connectionString))
-            {
                 conn.Open();
-            }
         }
 
         public void CreateHangfireSchema(string schemaName, string connectionString)
         {
             using (var conn = new SqlConnection(connectionString))
-            {
                 Hangfire.SqlServer.SqlServerObjectsInstaller.Install(conn, schemaName, true);
-            }
         }
     }
 }
