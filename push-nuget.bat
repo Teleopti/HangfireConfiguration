ecf43d60dd152984c79f07f12891729eed631898
Mathias Stenbom <mathias.stenbom@teleopti.com>
2019-10-16 10:21:13 +0200

    Removing nuget publishing info since we are using AppVeyor and nuget.org now

--- push-nuget.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set SolutionDir=%1
-set ConfigurationName=%2
-set ProjectDir=%3
-set Version=%4
-if "%ConfigurationName%"=="Release" nuget.exe push -Source "TeleoptiNugets" -ApiKey AzureDevOps %ProjectDir%bin\%ConfigurationName%\Hangfire.Configuration.%Version%.nupkg
