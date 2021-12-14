d8b1b0ed303a332a819396f7e96bf20ab8f76dfe
Jonas <jonas.nordh@calabrio.com>
2021-12-14 11:05:51 +0100

    Configure postgres appveyor to use intergrated security

--- postgres-configure-sspi.ps1
+++ postgres-configure-sspi.ps1
@@ -4,8 +4,6 @@
 write-host 'Start powershell script'
 $env:Path += ";c:\program files\PostgreSQL\13\bin"
 
-$currentLocation = Get-location -Path
-write-host 'Path was: ' $currentLocation
 Set-location -Path 'c:\program files\PostgreSQL\13\data\'
 
 # Add a user mapping for domain users (or local users if your computer is not in a domain) to your pg_ident.conf
@@ -32,9 +30,3 @@ $env:PGPASSWORD='Password12!';
 
 # Create a database user for the current Windows user who is a cluster admin with all rights
 createuser -dilrs --replication -U postgres
-
-Set-location -Path $currentLocation
-# Create an own database for the current Windows user for initial connection
-#createdb --maintenance-db=postgres
-# After this has successfully run, you should be able to connect to your cluster just by typing psql
-
