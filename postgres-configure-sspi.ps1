a20f78a7ce438332f4a1a343b37eb258bdb8677f
Jonas <jonas.nordh@calabrio.com>
2021-12-14 10:51:12 +0100

    Configure postgres appveyor to use intergrated security

--- postgres-configure-sspi.ps1
+++ postgres-configure-sspi.ps1
@@ -6,20 +6,20 @@ $env:Path += ";c:\program files\PostgreSQL\13\bin"
 Set-location -Path 'c:\program files\PostgreSQL\13\data\'
 
 # Add a user mapping for domain users (or local users if your computer is not in a domain) to your pg_ident.conf
-(Get-Content -Path pg_ident.conf -Encoding utf8NoBOM) `
+(Get-Content -Path pg_ident.conf) `
   -replace "# MAPNAME       SYSTEM-USERNAME         PG-USERNAME",`
            "# MAPNAME       SYSTEM-USERNAME         PG-USERNAME`r`nDomainOrLocalUser       /^(.*)@$env:USERDOMAIN         \1" | `
-  Set-Content -Path pg_ident.conf -Encoding utf8NoBOM
+  Set-Content -Path pg_ident.conf
 
 # Enable SSPI for the current user in your pg_hba.conf and reference the user mapping
-(Get-Content -Path pg_hba.conf -Encoding utf8NoBOM) `
+(Get-Content -Path pg_hba.conf ) `
   -replace "# `"local`" is for Unix domain socket connections only",`
            "# `"local`" is for Unix domain socket connections only`r`nlocal   all             $env:USERNAME                                    sspi map=DomainOrLocalUser" `
   -replace "# IPv4 local connections:",`
            "# IPv4 local connections:`r`nhost    all             $env:USERNAME            127.0.0.1/32            sspi map=DomainOrLocalUser" `
   -replace "# IPv6 local connections:",`
            "# IPv6 local connections:`r`nhost    all             $env:USERNAME            ::1/128                 sspi map=DomainOrLocalUser" | `
-  Set-Content -Path pg_hba.conf -Encoding utf8NoBOM
+  Set-Content -Path pg_hba.conf
 
 # The following commands will fail if the PostgreSQL bin directory is not in your PATH
 # Reload the configuration so that the changes above are in effect
