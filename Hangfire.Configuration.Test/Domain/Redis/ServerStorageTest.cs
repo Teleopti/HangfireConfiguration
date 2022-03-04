14e5630ecdb9ca46e8e8ea7c51fbeb920dd75b65
Roger Kratz <roger.kratz@gmail.com>
2022-03-04 10:27:54 +0100

    #109929 adding first redis test. to have something to reason about + make sure hangfire pro refs works

---
+++ Hangfire.Configuration.Test/Domain/Redis/ServerStorageTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Hangfire.Pro.Redis;
+using Xunit;
+
+namespace Hangfire.Configuration.Test.Domain.Redis
+{
+	public class ServerStorageTest
+	{
+		[Fact(Skip = "to be discussed/fixed")]
+		public void ShouldStartRedisServer()
+		{
+			var system = new SystemUnderTest();
+			system.ConfigurationStorage.Has(new StoredConfiguration {ConnectionString = "redis$$something"});
+			system.WorkerServerStarter.Start();
+			
+			Assert.IsType<RedisStorageOptions>(system.Hangfire.StartedServers.Single().storage.Options);
+		}
+	}
+}
