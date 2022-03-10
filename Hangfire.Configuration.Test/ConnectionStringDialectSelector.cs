0266496b43c88729cc181588ea135d25d0944f5c
Roger Kratz <roger.kratz@gmail.com>
2022-03-10 17:07:03 +0100

    #109929 fixing build

--- Hangfire.Configuration.Test/ConnectionStringDialectSelector.cs
+++ Hangfire.Configuration.Test/ConnectionStringDialectSelector.cs
@@ -1,10 +1,23 @@
 using System;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Reflection;
+using Hangfire.PostgreSql;
+using Hangfire.SqlServer;
 using Npgsql;
 
 namespace Hangfire.Configuration.Test
 {
+	//TODO: needed?
+	public static class DefaultSchemaName
+	{
+		public static string SqlServer() =>
+			typeof(SqlServerStorageOptions).Assembly.GetType("Hangfire.SqlServer.Constants")
+				.GetField("DefaultSchema", BindingFlags.Static | BindingFlags.Public).GetValue(null) as string;
+
+		public static string Postgres() => new PostgreSqlStorageOptions().SchemaName;
+	}
+	
 	//TODO - does this need to be kept?
 	public static class ConnectionStringExtensions
 	{
