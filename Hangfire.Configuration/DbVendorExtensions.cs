dd5775e819553ea3a8aa3ddc858ba305b3c69a4d
Roger Kratz <roger.kratz@gmail.com>
2022-03-10 16:01:23 +0100

    #109929 dialectselector refact #8 - removing DatabaseProviderExtensions

--- Hangfire.Configuration/DbVendorExtensions.cs
+++ Hangfire.Configuration/DbVendorExtensions.cs
@@ -12,7 +12,7 @@ public interface IDbVendorSelector
 
 internal static class DbVendorExtensions
 {
-	public const string RedisStart = "redis$$";
+	private const string RedisStart = "redis$$";
 	
 	public static IDbVendorSelector ToDbVendorSelector(this string connectionString) => 
 		new connectionStringDialectSelector(connectionString);
@@ -46,6 +46,57 @@ internal static class DbVendorExtensions
 	{
 		return selector.SelectDialect(() => sqlServer, () => postgres, () => redis);
 	}
+	
+	public static string TrimRedisPrefix(this string connectionString)
+	{
+		return connectionString.ToDbVendorSelector().SelectDialect(
+			() => connectionString,
+			() => connectionString,
+			() => connectionString.Substring(RedisStart.Length)) 
+		       ?? connectionString;
+	}
+	
+	public static string DatabaseName(this string connectionString)
+	{
+		return connectionString.ToDbVendorSelector().SelectDialect(
+			() => new SqlConnectionStringBuilder(connectionString).InitialCatalog,
+			() => new NpgsqlConnectionStringBuilder(connectionString).Database);
+	}
+
+	public static string ApplicationName(this string connectionString)
+	{
+		return connectionString.ToDbVendorSelector().SelectDialect(
+			() => new SqlConnectionStringBuilder(connectionString).ApplicationName,
+			() => new NpgsqlConnectionStringBuilder(connectionString).ApplicationName);
+	}
+
+	public static string PointToMasterDatabase(this string connectionString)
+	{
+		return connectionString.ToDbVendorSelector().SelectDialect(
+			() => new SqlConnectionStringBuilder(connectionString) {InitialCatalog = "master"}.ToString(),
+			() => new NpgsqlConnectionStringBuilder(connectionString) {Database = "postgres"}.ToString());
+	}
+
+	public static string ChangeDatabase(this string connectionString, string newDatabase)
+	{
+		return connectionString.ToDbVendorSelector().SelectDialect(
+			() => new SqlConnectionStringBuilder(connectionString) {InitialCatalog = newDatabase}.ToString(),
+			() => new NpgsqlConnectionStringBuilder(connectionString) {Database = newDatabase}.ToString());
+	}
+
+	public static string ChangeApplicationName(this string connectionString, string applicationName) =>
+		connectionString.ToDbVendorSelector().SelectDialect(
+			() => new SqlConnectionStringBuilder(connectionString) {ApplicationName = applicationName}.ToString(),
+			() => new NpgsqlConnectionStringBuilder(connectionString) {ApplicationName = applicationName}.ToString(),
+			() => connectionString);
+
+	public static DbConnection CreateConnection(this string connectionString)
+	{
+		var connection = connectionString.ToDbVendorSelector().SelectDialect<DbConnection>(
+			() => new SqlConnection(), () => new NpgsqlConnection());
+		connection.ConnectionString = connectionString;
+		return connection;
+	}
 		
 	private class connectionStringDialectSelector : IDbVendorSelector
 	{
@@ -69,7 +120,7 @@ internal static class DbVendorExtensions
 
 		private bool isRedis()
 		{
-			return _connectionString.StartsWith(RedisStart);
+			return _connectionString != null && _connectionString.StartsWith(RedisStart);
 		}
 
 		private bool isSqlServer()
