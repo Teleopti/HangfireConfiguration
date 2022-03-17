4a5e58cbddae4511142f012feadbfce4c08be330
Roger Kratz <roger.kratz@gmail.com>
2022-03-17 16:33:31 +0100

    #109929 enqueue test when redis is storage

---
+++ Hangfire.Configuration.Test/Integration/QueueWorkTest_Redis.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Hangfire.Server;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace Hangfire.Configuration.Test.Integration;
+
+public class QueueWorkTest_Redis
+{
+	public class FakeJobService
+	{
+		public static void Reset() => WasRun = false;
+		public static bool WasRun = false;
+		public static void RunTheJob() => WasRun = true;
+	}
+	
+	private Process redis;
+	
+	[SetUp]
+	public void Setup()
+	{
+		DatabaseTestSetup.SetupSqlServer(ConnectionStrings.SqlServer);
+		redis = Process.Start($"{Environment.GetEnvironmentVariable("USERPROFILE")}/.nuget/packages/redis-64/3.0.503/tools/redis-server.exe");
+	}
+	
+	[TearDown]
+	public void Teardown()
+	{
+		redis.Kill();
+	}
+	
+	[Test]
+	public void ShouldRunEnqueuedJobOnWorker()
+	{
+	
+		var system = new SystemUnderInfraTest();
+		system.UseOptions(new ConfigurationOptions
+		{
+			ConnectionString = ConnectionStrings.SqlServer,
+			UpdateConfigurations = new[]
+			{
+				new UpdateStorageConfiguration
+				{
+					ConnectionString = "localhost",
+					Name = DefaultConfigurationName.Name()
+				}
+			}
+		});
+		system.UseRealHangfire();
+		FakeJobService.Reset();
+
+		var storage = system.BuildPublishersQuerier()
+			.QueryPublishers().Single().JobStorage;
+		var client = new BackgroundJobClient(storage);
+		client.Enqueue(() => FakeJobService.RunTheJob());
+		emulateWorkerIteration(storage);
+
+		FakeJobService.WasRun.Should().Be.True();
+	}
+
+	private static void emulateWorkerIteration(JobStorage storage)
+	{
+		// will hang if nothing to work with
+		// if (NumberOfEnqueuedJobs() > 0)
+		new Worker().Execute(
+			new BackgroundProcessContext(
+				"fake server",
+				storage,
+				new Dictionary<string, object>(),
+				Guid.NewGuid(),
+				new CancellationToken(),
+				new CancellationToken(),
+				new CancellationToken()
+			));
+	}
+}
