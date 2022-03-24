5dfe4f4241ca68e63c4d6cf283307adacbcb5a1c
Roger Kratz <roger.kratz@gmail.com>
2022-03-24 15:04:39 +0100

    #109929 Check that redis server has correct maxmemory_policy setting

--- Hangfire.Configuration/Internals/RedisConfigurationVerifier.cs
+++ Hangfire.Configuration/Internals/RedisConfigurationVerifier.cs
@@ -8,9 +8,18 @@ internal class RedisConfigurationVerifier : IRedisConfigurationVerifier
 {
 	public void VerifyConfiguration(string configuration, string prefix)
 	{
-		var redis = ConnectionMultiplexer.Connect(configuration);
+		var redis = ConnectionMultiplexer.Connect(configuration+",allowAdmin=true");
 
-		if (redis.GetEndPoints().Any(endPoint => redis.GetServer(endPoint).Keys(pattern: $"{prefix}*").Any()))
-			throw new ArgumentException($"Prefix '{prefix}' already in use!");
+		foreach (var endPoint in redis.GetEndPoints())
+		{
+			if(redis.GetServer(endPoint).Keys(pattern: $"{prefix}*").Any())
+				throw new ArgumentException($"Prefix '{prefix}' already in use!");
+			
+			// no coverage, not applicable to our test redis server
+			var memPolicy = redis.GetServer(endPoint).Info("memory")[0].SingleOrDefault(x => x.Key=="maxmemory_policy");
+			if (memPolicy.Value != null && memPolicy.Value != "noeviction")
+				throw new ArgumentException("maxmemory_policy must be set to 'noeviction'!");
+			//
+		}
 	}
 }
