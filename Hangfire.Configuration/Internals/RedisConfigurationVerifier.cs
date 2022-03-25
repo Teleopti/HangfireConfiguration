f8d60b377c6c90d0e18e5fc894da90b151016599
Roger Kratz <roger.kratz@gmail.com>
2022-03-25 17:22:58 +0100

    minor improvement of ex message

--- Hangfire.Configuration/Internals/RedisConfigurationVerifier.cs
+++ Hangfire.Configuration/Internals/RedisConfigurationVerifier.cs
@@ -18,7 +18,7 @@ internal class RedisConfigurationVerifier : IRedisConfigurationVerifier
 			// no coverage, not applicable to our test redis server
 			var memPolicy = redis.GetServer(endPoint).Info("memory")[0].SingleOrDefault(x => x.Key=="maxmemory_policy");
 			if (memPolicy.Value != null && memPolicy.Value != "noeviction")
-				throw new ArgumentException("maxmemory_policy must be set to 'noeviction'!");
+				throw new ArgumentException($"maxmemory_policy must be set to 'noeviction' (but was {memPolicy.Value})!");
 			//
 		}
 	}
