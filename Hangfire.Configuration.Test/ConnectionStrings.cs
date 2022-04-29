94bce43011e35ce68f90df4fbe0313cb0e987744
Mathias Stenbom <mathias.stenbom@calabrio.com>
2022-04-29 09:44:22 +0200

    Appveyor pipe. Postgres password.

--- Hangfire.Configuration.Test/ConnectionStrings.cs
+++ Hangfire.Configuration.Test/ConnectionStrings.cs
@@ -3,6 +3,6 @@ namespace Hangfire.Configuration.Test
 	public static class ConnectionStrings
 	{
 		public const string SqlServer = @"Data Source=.;Initial Catalog=Hangfire.Configuration.Tests;Integrated Security=True;";
-		public const string Postgres = @"Host=localhost;Database=Hangfire.Configuration.Tests;Username=postgres;Password=root;Pooling=false;";
+		public const string Postgres = @"Host=localhost;Database=Hangfire.Configuration.Tests;Username=postgres;Password=Password12!;Pooling=false;";
 	}
 }
