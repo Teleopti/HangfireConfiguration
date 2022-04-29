6745a66e20b47b0d744e77e19af4c1741276fe90
Mathias Stenbom <mathias.stenbom@calabrio.com>
2022-04-29 09:23:50 +0200

    Revert "Setup sql server in github pipe. Testing if its a connection string issue."
    This reverts commit e7cfc7e0f89ace7379b8cd8f0f80caa861b5f59a.

--- Hangfire.Configuration.Test/ConnectionStrings.cs
+++ Hangfire.Configuration.Test/ConnectionStrings.cs
@@ -2,8 +2,7 @@ namespace Hangfire.Configuration.Test
 {
 	public static class ConnectionStrings
 	{
-		// public const string SqlServer = @"Data Source=.;Initial Catalog=Hangfire.Configuration.Tests;Integrated Security=True;";
-		public const string SqlServer = @"Data Source=localhost;Initial Catalog=Hangfire.Configuration.Tests;User Id=sa;Password=P@ssw0rd;";
+		public const string SqlServer = @"Data Source=.;Initial Catalog=Hangfire.Configuration.Tests;Integrated Security=True;";
 		public const string Postgres = @"Host=localhost;Database=Hangfire.Configuration.Tests;Username=postgres;Password=root;Pooling=false;";
 	}
 }
