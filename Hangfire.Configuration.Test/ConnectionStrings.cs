8f1df83b9fd3897ad1ee44cf626ac007e4feb9c2
Mathias Stenbom <mathias.stenbom@calabrio.com>
2022-05-04 16:49:54 +0200

    Github pipe.

--- Hangfire.Configuration.Test/ConnectionStrings.cs
+++ Hangfire.Configuration.Test/ConnectionStrings.cs
@@ -5,8 +5,7 @@ namespace Hangfire.Configuration.Test
 #if DEBUG
 		public const string SqlServer = @"Data Source=.;Initial Catalog=Hangfire.Configuration.Tests;Integrated Security=True;";
 #else
-		//public const string SqlServer = @"Data Source=localhost;Initial Catalog=Hangfire.Configuration.Tests;User Id=sa;Password=P@ssw0rd";
-		public const string SqlServer = @"Data Source=.\SQLEXPRESS;Initial Catalog=Hangfire.Configuration.Tests;Integrated Security=True;";
+		public const string SqlServer = @"Data Source=localhost;Initial Catalog=Hangfire.Configuration.Tests;User Id=sa;Password=P@ssw0rd2";
 #endif
 		
 		public const string Postgres = @"Host=localhost;Database=Hangfire.Configuration.Tests;Username=postgres;Password=root;Pooling=false;";
--- Hangfire.Configuration.Test/DatabaseTestSetup.cs
+++ Hangfire.Configuration.Test/DatabaseTestSetup.cs
@@ -2,20 +2,13 @@ using System.Data.Common;
 using System.Data.SqlClient;
 using Dapper;
 using Npgsql;
-using NUnit.Framework;
 
 namespace Hangfire.Configuration.Test;
 
 public static class DatabaseTestSetup
 {
-	private static int _postgresTestCount = 0;
-
 	public static void SetupPostgres(string connectionString, int? schemaVersion = null)
 	{
-		if (_postgresTestCount >= int.MaxValue)
-			Assert.Ignore("Skipping the rest of postgres");
-		_postgresTestCount++;
-
 		var database = new NpgsqlConnectionStringBuilder(connectionString).Database;
 		var master = new NpgsqlConnectionStringBuilder(connectionString) {Database = "postgres"}.ToString();
 		using (var c = new NpgsqlConnection(master))
@@ -27,15 +20,9 @@ public static class DatabaseTestSetup
 		using (var conn = new NpgsqlConnection(connectionString))
 			installSchema(conn, schemaVersion);
 	}
-
-	private static int _sqlServerTestCount = 0;
-
+	
 	public static void SetupSqlServer(string connectionString, int? schemaVersion = null)
 	{
-		if (_sqlServerTestCount >= int.MaxValue)
-			Assert.Ignore("Skipping the rest of sql server");
-		_sqlServerTestCount++;
-
 		var database = new SqlConnectionStringBuilder(connectionString).InitialCatalog;
 		var master = new SqlConnectionStringBuilder(connectionString) {InitialCatalog = "master"}.ToString();
 		using (var c = new SqlConnection(master))
@@ -44,7 +31,6 @@ public static class DatabaseTestSetup
 			c.Execute($"DROP DATABASE IF EXISTS [{database}]");
 			c.Execute($"CREATE DATABASE [{database}]");
 		}
-
 		using (var conn = new SqlConnection(connectionString))
 			installSchema(conn, schemaVersion);
 	}
