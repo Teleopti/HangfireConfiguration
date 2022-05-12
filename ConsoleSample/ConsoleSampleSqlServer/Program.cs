9bad6658fd6bffc78873ef42816bd514c288714b
Mathias Stenbom <mathias.stenbom@calabrio.com>
2022-05-12 21:19:25 +0200

    Adding redis support to console sample applications. Minor enqueue improvements.

--- ConsoleSample/ConsoleSampleSqlServer/Program.cs
+++ ConsoleSample/ConsoleSampleSqlServer/Program.cs
@@ -11,90 +11,90 @@ using Microsoft.Owin.Hosting;
 
 namespace ConsoleSample
 {
-    public static class Program
-    {
-        public static string NodeAddress = "http://localhost:12345";
-        
-        public static void Main()
-        {
-            Console.WriteLine("Starting site on " + NodeAddress);
-            
+	public static class Program
+	{
+		public static string NodeAddress = "http://localhost:12345";
+
+		public static void Main()
+		{
+			Console.WriteLine("Starting site on " + NodeAddress);
+
 #if !NET472
-            using (var host = new WebHostBuilder()
-                .UseUrls(NodeAddress)
-                .UseStartup<Startup>()
-                .UseKestrel()
-                .Build())
-            {
-                host.Start();
-                mainLoop(Startup.HangfireConfiguration);
-            }
+			using (var host = new WebHostBuilder()
+				       .UseUrls(NodeAddress)
+				       .UseStartup<Startup>()
+				       .UseKestrel()
+				       .Build())
+			{
+				host.Start();
+				mainLoop(Startup.HangfireConfiguration);
+			}
 #else
             using (WebApp.Start<Startup>(NodeAddress))
                 mainLoop(Startup.HangfireConfiguration);
 #endif
 
-            Console.WriteLine("Press Enter to exit...");
-            Console.ReadLine();
-        }
+			Console.WriteLine("Press Enter to exit...");
+			Console.ReadLine();
+		}
 
-        private static void mainLoop(HangfireConfiguration hangfireConfiguration)
-        {
-            Console.WriteLine("Started.");
-            Console.WriteLine("'stop' to exit.");
-            Console.WriteLine("'add 1' to queue a job.");
-            while (true)
-            {
-                var command = Console.ReadLine();
+		private static void mainLoop(HangfireConfiguration hangfireConfiguration)
+		{
+			Console.WriteLine("Started.");
+			Console.WriteLine("'stop' to exit.");
+			Console.WriteLine("'add 1' to queue a job.");
+			while (true)
+			{
+				var command = Console.ReadLine();
 
-                if (command == null || command.Equals("stop", StringComparison.OrdinalIgnoreCase))
-                {
-                    break;
-                }
+				if (command == null || command.Equals("stop", StringComparison.OrdinalIgnoreCase))
+				{
+					break;
+				}
 
-                if (command.StartsWith("add", StringComparison.OrdinalIgnoreCase))
-                {
-                    try
-                    {
-                        var workCount = int.Parse(command.Substring(4));
-                        for (var i = 0; i < workCount; i++)
-                        {
-                            var number = i;
-                            var client = new BackgroundJobClient(hangfireConfiguration.QueryPublishers().First().JobStorage);
-                            client.Enqueue<Services>(x => x.Random(number));
-                        }
+				if (command.StartsWith("add", StringComparison.OrdinalIgnoreCase))
+				{
+					try
+					{
+						var publisher = hangfireConfiguration.QueryPublishers().First();
+						var workCount = int.Parse(command.Substring(4));
+						for (var i = 0; i < workCount; i++)
+						{
+							var number = i;
+							publisher.BackgroundJobClient.Enqueue<Services>(x => x.Random(number));
+						}
 
-                        Console.WriteLine("Jobs enqueued.");
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                }
-            }
-        }
-    }
+						Console.WriteLine("Jobs enqueued on " + publisher.ConfigurationId);
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+				}
+			}
+		}
+	}
 
-    public class Services
-    {
-        private static readonly Random Rand = new Random();
+	public class Services
+	{
+		private static readonly Random Rand = new Random();
 
-        [Queue("critical")]
-        public void Random(int number)
-        {
-            int time;
-            lock (Rand)
-            {
-                time = Rand.Next(10);
-            }
+		[Queue("critical")]
+		public void Random(int number)
+		{
+			int time;
+			lock (Rand)
+			{
+				time = Rand.Next(10);
+			}
 
-            if (time < 5)
-            {
-                throw new Exception();
-            }
+			if (time < 5)
+			{
+				throw new Exception();
+			}
 
-            Thread.Sleep(TimeSpan.FromSeconds(5 + time));
-            Console.WriteLine("Finished task: " + number);
-        }
-    }
+			Thread.Sleep(TimeSpan.FromSeconds(5 + time));
+			Console.WriteLine("Finished task: " + number);
+		}
+	}
 }
