9bd35d94260416a37478d19f41ce7d8f4b972e69
Mathias Stenbom <mathias.stenbom@calabrio.com>
2022-11-14 13:15:44 +0100

    Going back to windows and choco

--- Hangfire.Configuration.Test/ConnectionStrings.cs
+++ Hangfire.Configuration.Test/ConnectionStrings.cs
@@ -5,7 +5,8 @@ namespace Hangfire.Configuration.Test
 #if DEBUG
 		public const string SqlServer = @"Data Source=.;Initial Catalog=Hangfire.Configuration.Tests;Integrated Security=True;";
 #else
-		public const string SqlServer = @"Data Source=localhost;Initial Catalog=Hangfire.Configuration.Tests;User Id=sa;Password=P@ssw0rd2";
+		public const string SqlServer = @"Data Source=.;Initial Catalog=Hangfire.Configuration.Tests;Integrated Security=True;";
+		// public const string SqlServer = @"Data Source=localhost;Initial Catalog=Hangfire.Configuration.Tests;User Id=sa;Password=P@ssw0rd2";
 #endif
 		
 		public const string Postgres = @"Host=localhost;Database=Hangfire.Configuration.Tests;Username=postgres;Password=root;Pooling=false;";
