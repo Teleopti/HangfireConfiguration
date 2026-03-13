d9223c1288b4e95567931f7380b436718dfde8c2
Mathias Stenbom <mathias.stenbom@calabrio.com>
2026-03-13 09:19:45 +0100

    Namespace reformat

--- Hangfire.Configuration.Test/Domain/WorkerBalancerWithGracefulShutdownTest.cs
+++ Hangfire.Configuration.Test/Domain/WorkerBalancerWithGracefulShutdownTest.cs
@@ -1,79 +1,77 @@
 using System.Linq;
-using Hangfire.Configuration.Test.Domain.Fake;
 using NUnit.Framework;
 
-namespace Hangfire.Configuration.Test.Domain
+namespace Hangfire.Configuration.Test.Domain;
+
+public class WorkerBalancerWithGracefulShutdownTest
 {
-    public class WorkerBalancerWithGracefulShutdownTest
+    [Test]
+    public void ShouldCalculateWorkersOnStartOfSecondServer()
     {
-        [Test]
-        public void ShouldCalculateWorkersOnStartOfSecondServer()
-        {
-            var system = new SystemUnderTest();
-            system.HasGoalWorkerCount(8);
-            system.Monitor.AnnounceServer("runningServer");
+        var system = new SystemUnderTest();
+        system.HasGoalWorkerCount(8);
+        system.Monitor.AnnounceServer("runningServer");
 
-            system.WorkerServerStarter.Start();
+        system.WorkerServerStarter.Start();
 
-            Assert.AreEqual(4, system.Hangfire.StartedServers.Single().options.WorkerCount);
-        }
-        
-        [Test]
-        public void ShouldCalculateWorkersOnStartOfThirdServer()
-        {
-            var system = new SystemUnderTest();
-            system.HasGoalWorkerCount(9);
-            system.Monitor.AnnounceServer("server1");
-            system.Monitor.AnnounceServer("server2");
-
-            system.WorkerServerStarter.Start();
-
-            Assert.AreEqual(3, system.Hangfire.StartedServers.Single().options.WorkerCount);
-        }
-        
-        [Test]
-        public void ShouldRoundWorkerCountUp()
-        {
-            var system = new SystemUnderTest();
-            system.HasGoalWorkerCount(10);
-            system.Monitor.AnnounceServer("server1");
-            system.Monitor.AnnounceServer("server2");
-            system.Monitor.AnnounceServer("server3");
-
-            system.WorkerServerStarter.Start();
-
-            Assert.AreEqual(3, system.Hangfire.StartedServers.Single().options.WorkerCount);
-        }
-        
-        [Test]
-        public void ShouldCalculateBasedOnMaxOneHundredWhenStartingFourthServer()
-        {
-            var system = new SystemUnderTest();
-            system.HasGoalWorkerCount(200);
-            system.Monitor.AnnounceServer("server1");
-            system.Monitor.AnnounceServer("server2");
-            system.Monitor.AnnounceServer("server3");
-
-            system.WorkerServerStarter.Start();
-
-            Assert.AreEqual(25, system.Hangfire.StartedServers.Single().options.WorkerCount);
-        }
-
-        [Test]
-        public void ShouldCalculateGoalBasedOnMinimumKnownOnStartOfThirdServer()
-        {
-            var system = new SystemUnderTest();
-            system.HasGoalWorkerCount(100);
-            system.Monitor.AnnounceServer("server1");
-            system.Monitor.AnnounceServer("server2");
-
-            system.WorkerServerStarter.Start(new ConfigurationOptionsForTest
-            {
-                MinimumServerCount = 4
-			});
-
-            Assert.AreEqual(25, system.Hangfire.StartedServers.Single().options.WorkerCount);
-        }        
-        
+        Assert.AreEqual(4, system.Hangfire.StartedServers.Single().options.WorkerCount);
     }
+        
+    [Test]
+    public void ShouldCalculateWorkersOnStartOfThirdServer()
+    {
+        var system = new SystemUnderTest();
+        system.HasGoalWorkerCount(9);
+        system.Monitor.AnnounceServer("server1");
+        system.Monitor.AnnounceServer("server2");
+
+        system.WorkerServerStarter.Start();
+
+        Assert.AreEqual(3, system.Hangfire.StartedServers.Single().options.WorkerCount);
+    }
+        
+    [Test]
+    public void ShouldRoundWorkerCountUp()
+    {
+        var system = new SystemUnderTest();
+        system.HasGoalWorkerCount(10);
+        system.Monitor.AnnounceServer("server1");
+        system.Monitor.AnnounceServer("server2");
+        system.Monitor.AnnounceServer("server3");
+
+        system.WorkerServerStarter.Start();
+
+        Assert.AreEqual(3, system.Hangfire.StartedServers.Single().options.WorkerCount);
+    }
+        
+    [Test]
+    public void ShouldCalculateBasedOnMaxOneHundredWhenStartingFourthServer()
+    {
+        var system = new SystemUnderTest();
+        system.HasGoalWorkerCount(200);
+        system.Monitor.AnnounceServer("server1");
+        system.Monitor.AnnounceServer("server2");
+        system.Monitor.AnnounceServer("server3");
+
+        system.WorkerServerStarter.Start();
+
+        Assert.AreEqual(25, system.Hangfire.StartedServers.Single().options.WorkerCount);
+    }
+
+    [Test]
+    public void ShouldCalculateGoalBasedOnMinimumKnownOnStartOfThirdServer()
+    {
+        var system = new SystemUnderTest();
+        system.HasGoalWorkerCount(100);
+        system.Monitor.AnnounceServer("server1");
+        system.Monitor.AnnounceServer("server2");
+
+        system.WorkerServerStarter.Start(new ConfigurationOptionsForTest
+        {
+            MinimumServerCount = 4
+        });
+
+        Assert.AreEqual(25, system.Hangfire.StartedServers.Single().options.WorkerCount);
+    }        
+        
 }
