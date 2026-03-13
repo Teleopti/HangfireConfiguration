bde158e22d152a96ce3bed43657f3e8073b3427b
Mathias Stenbom <mathias.stenbom@calabrio.com>
2026-03-13 11:29:32 +0100

    Namespace reformats

--- Hangfire.Configuration.Test/Domain/ConfigureUpdateConfigurationsRedisTest.cs
+++ Hangfire.Configuration.Test/Domain/ConfigureUpdateConfigurationsRedisTest.cs
@@ -1,29 +1,28 @@
 ﻿using System.Linq;
 using NUnit.Framework;
 
-namespace Hangfire.Configuration.Test.Domain
-{
-	public class ConfigureUpdateConfigurationsRedisTest
-	{
-		[Test]
-		public void ShouldUseRedisOptions()
-		{
-			var system = new SystemUnderTest();
-			system.WorkerServerStarter.Start(new ConfigurationOptions
-			{
-				ExternalConfigurations = new[]
-				{
-					new ExternalConfiguration
-					{
-						Name = "redis",
-						ConnectionString = "redis$$connstring",
-					}
-				}
-			});
+namespace Hangfire.Configuration.Test.Domain;
 
-			var configuration = system.Configurations().Single();
-			Assert.AreEqual("redis", configuration.Name);
-			Assert.AreEqual("redis$$connstring", configuration.ConnectionString);
-		}
+public class ConfigureUpdateConfigurationsRedisTest
+{
+	[Test]
+	public void ShouldUseRedisOptions()
+	{
+		var system = new SystemUnderTest();
+		system.WorkerServerStarter.Start(new ConfigurationOptions
+		{
+			ExternalConfigurations = new[]
+			{
+				new ExternalConfiguration
+				{
+					Name = "redis",
+					ConnectionString = "redis$$connstring",
+				}
+			}
+		});
+
+		var configuration = system.Configurations().Single();
+		Assert.AreEqual("redis", configuration.Name);
+		Assert.AreEqual("redis$$connstring", configuration.ConnectionString);
 	}
 }
--- Hangfire.Configuration.Test/Domain/ConfigureUpdateConfigurationsTest.cs
+++ Hangfire.Configuration.Test/Domain/ConfigureUpdateConfigurationsTest.cs
@@ -1,111 +1,110 @@
 using System.Linq;
 using NUnit.Framework;
 
-namespace Hangfire.Configuration.Test.Domain
+namespace Hangfire.Configuration.Test.Domain;
+
+public class ConfigureUpdateConfigurationsTest
 {
-    public class ConfigureUpdateConfigurationsTest
-    {
-        [Test]
-        public void ShouldConfigureUpdatedConfiguration()
-        {
-            var system = new SystemUnderTest();
+	[Test]
+	public void ShouldConfigureUpdatedConfiguration()
+	{
+		var system = new SystemUnderTest();
 
-            system.UseOptions(new ConfigurationOptionsForTest
-            {
-	            ExternalConfigurations = new[]
-	            {
-		            new ExternalConfiguration
-		            {
-			            Name = "name",
-			            ConnectionString = "connectionString",
-			            SchemaName = "schema"
-		            }
-	            }
-            });
-            system.WorkerServerStarter.Start();
+		system.UseOptions(new ConfigurationOptionsForTest
+		{
+			ExternalConfigurations = new[]
+			{
+				new ExternalConfiguration
+				{
+					Name = "name",
+					ConnectionString = "connectionString",
+					SchemaName = "schema"
+				}
+			}
+		});
+		system.WorkerServerStarter.Start();
 
-            var configuration = system.Configurations().Single();
-            Assert.AreEqual("name", configuration.Name);
-            Assert.AreEqual("connectionString", configuration.ConnectionString);
-            Assert.AreEqual("schema", configuration.SchemaName);
-        }
+		var configuration = system.Configurations().Single();
+		Assert.AreEqual("name", configuration.Name);
+		Assert.AreEqual("connectionString", configuration.ConnectionString);
+		Assert.AreEqual("schema", configuration.SchemaName);
+	}
 
-        [Test]
-        public void ShouldActivateOnFirstUpdate()
-        {
-            var system = new SystemUnderTest();
+	[Test]
+	public void ShouldActivateOnFirstUpdate()
+	{
+		var system = new SystemUnderTest();
 
-            system.UseOptions(new ConfigurationOptionsForTest
-            {
-	            ExternalConfigurations = new[]
-	            {
-		            new ExternalConfiguration
-		            {
-			            Name = "name",
-			            ConnectionString = "connectionString"
-		            }
-	            }
-            });
-            system.WorkerServerStarter.Start();
+		system.UseOptions(new ConfigurationOptionsForTest
+		{
+			ExternalConfigurations = new[]
+			{
+				new ExternalConfiguration
+				{
+					Name = "name",
+					ConnectionString = "connectionString"
+				}
+			}
+		});
+		system.WorkerServerStarter.Start();
 
-            Assert.True(system.Configurations().Single().Active);
-        }
+		Assert.True(system.Configurations().Single().Active);
+	}
 
-        [Test]
-        public void ShouldUpdateConfiguration()
-        {
-            var system = new SystemUnderTest();
-            system.WithConfiguration(new StoredConfiguration {Name = "name", ConnectionString = "previous"});
+	[Test]
+	public void ShouldUpdateConfiguration()
+	{
+		var system = new SystemUnderTest();
+		system.WithConfiguration(new StoredConfiguration {Name = "name", ConnectionString = "previous"});
 
-            system.UseOptions(new ConfigurationOptionsForTest
-            {
-	            ExternalConfigurations = new[]
-	            {
-		            new ExternalConfiguration
-		            {
-			            Name = "name",
-			            ConnectionString = "newConnectionString"
-		            }
-	            }
-            });
-            system.WorkerServerStarter.Start();
+		system.UseOptions(new ConfigurationOptionsForTest
+		{
+			ExternalConfigurations = new[]
+			{
+				new ExternalConfiguration
+				{
+					Name = "name",
+					ConnectionString = "newConnectionString"
+				}
+			}
+		});
+		system.WorkerServerStarter.Start();
 
-            var configuration = system.Configurations().Single();
-            Assert.AreEqual("newConnectionString", configuration.ConnectionString);
-        }
+		var configuration = system.Configurations().Single();
+		Assert.AreEqual("newConnectionString", configuration.ConnectionString);
+	}
 
-        [Test]
-        public void ShouldConfigureUpdatedConfigurations()
-        {
-            var system = new SystemUnderTest();
+	[Test]
+	public void ShouldConfigureUpdatedConfigurations()
+	{
+		var system = new SystemUnderTest();
 
-            system.UseOptions(new ConfigurationOptionsForTest
-            {
-	            ExternalConfigurations = new[]
-	            {
-		            new ExternalConfiguration
-		            {
-			            Name = "name1",
-			            ConnectionString = "connectionString1",
-			            SchemaName = "schema1"
-		            },
-		            new ExternalConfiguration
-		            {
-			            Name = "name2",
-			            ConnectionString = "connectionString2",
-			            SchemaName = "schema2"
-		            }
-	            }
-            });
-            system.WorkerServerStarter.Start();
+		system.UseOptions(new ConfigurationOptionsForTest
+		{
+			ExternalConfigurations = new[]
+			{
+				new ExternalConfiguration
+				{
+					Name = "name1",
+					ConnectionString = "connectionString1",
+					SchemaName = "schema1"
+				},
+				new ExternalConfiguration
+				{
+					Name = "name2",
+					ConnectionString = "connectionString2",
+					SchemaName = "schema2"
+				}
+			}
+		});
+		system.WorkerServerStarter.Start();
 
-            var configuration = system.Configurations().OrderBy(x => x.Id);
-            Assert.AreEqual("name1", configuration.ElementAt(0).Name);
-            Assert.AreEqual("connectionString1", configuration.ElementAt(0).ConnectionString);
-            Assert.AreEqual("schema1", configuration.ElementAt(0).SchemaName);
-            Assert.AreEqual("name2", configuration.ElementAt(1).Name);
-            Assert.AreEqual("connectionString2", configuration.ElementAt(1).ConnectionString);
-            Assert.AreEqual("schema2", configuration.ElementAt(1).SchemaName);
-        }
-    }
+		var configuration = system.Configurations().OrderBy(x => x.Id);
+		Assert.AreEqual("name1", configuration.ElementAt(0).Name);
+		Assert.AreEqual("connectionString1", configuration.ElementAt(0).ConnectionString);
+		Assert.AreEqual("schema1", configuration.ElementAt(0).SchemaName);
+		Assert.AreEqual("name2", configuration.ElementAt(1).Name);
+		Assert.AreEqual("connectionString2", configuration.ElementAt(1).ConnectionString);
+		Assert.AreEqual("schema2", configuration.ElementAt(1).SchemaName);
+	}
 }
