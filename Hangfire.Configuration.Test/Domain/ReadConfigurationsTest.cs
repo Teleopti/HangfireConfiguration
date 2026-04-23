0e069691ffe2360a2abd3bf68179139c07be56c3
Mathias Stenbom <mathias.stenbom@calabrio.com>
2026-04-23 10:05:48 +0200

    Reformat

--- Hangfire.Configuration.Test/Domain/ReadConfigurationsTest.cs
+++ Hangfire.Configuration.Test/Domain/ReadConfigurationsTest.cs
@@ -6,68 +6,68 @@ namespace Hangfire.Configuration.Test.Domain;
 
 public class ReadConfigurationsTest
 {
-    [Test]
-    public void ShouldRead()
-    {
-        var system = new SystemUnderTest();
-        system.WithConfiguration(new StoredConfiguration
-        {
-            Id = 13,
-            ConnectionString = "connection",
-            SchemaName = "schema",
-            Active = true,
-            Containers = new[] { new ContainerConfiguration { GoalWorkerCount = 11 } }
-        });
+	[Test]
+	public void ShouldRead()
+	{
+		var system = new SystemUnderTest();
+		system.WithConfiguration(new StoredConfiguration
+		{
+			Id = 13,
+			ConnectionString = "connection",
+			SchemaName = "schema",
+			Active = true,
+			Containers = new[] {new ContainerConfiguration {GoalWorkerCount = 11}}
+		});
 
-        var result = system.ConfigurationApi().ReadConfigurations() as StoredConfiguration[];
+		var result = system.ConfigurationApi().ReadConfigurations() as StoredConfiguration[];
 
-        Assert.AreEqual(13, result.Single().Id);
-        Assert.AreEqual("connection", result.Single().ConnectionString);
-        Assert.AreEqual("schema", result.Single().SchemaName);
-        Assert.AreEqual(true, result.Single().Active);
-        Assert.AreEqual(11, result.Single().Containers.Single().GoalWorkerCount);
-    }
-        
-    [Test]
-    public void ShouldWrite()
-    {
-        var system = new SystemUnderTest();
+		Assert.AreEqual(13, result.Single().Id);
+		Assert.AreEqual("connection", result.Single().ConnectionString);
+		Assert.AreEqual("schema", result.Single().SchemaName);
+		Assert.AreEqual(true, result.Single().Active);
+		Assert.AreEqual(11, result.Single().Containers.Single().GoalWorkerCount);
+	}
 
-        system.ConfigurationApi().WriteConfiguration(new StoredConfiguration
-        {
-            Id = 22,
-            ConnectionString = "connection",
-            SchemaName = "SchemaName",
-            Active = true,
-            Containers = new[] { new ContainerConfiguration { GoalWorkerCount = 44 } }
-        });
+	[Test]
+	public void ShouldWrite()
+	{
+		var system = new SystemUnderTest();
 
-        var result = system.ConfigurationApi().ReadConfigurations().Single();
-        Assert.AreEqual(22, result.Id);
-        Assert.AreEqual("connection", result.ConnectionString);
-        Assert.AreEqual("SchemaName", result.SchemaName);
-        Assert.True(result.Active);
-        Assert.AreEqual(44, result.Containers.Single().GoalWorkerCount);
-    }
-        
-    [Test]
-    public void ShouldDelete()
-    {
-        var system = new SystemUnderTest();
+		system.ConfigurationApi().WriteConfiguration(new StoredConfiguration
+		{
+			Id = 22,
+			ConnectionString = "connection",
+			SchemaName = "SchemaName",
+			Active = true,
+			Containers = new[] {new ContainerConfiguration {GoalWorkerCount = 44}}
+		});
 
-        system.ConfigurationApi().WriteConfiguration(new StoredConfiguration
-        {
-            Id = 3,
-            ConnectionString = "c",
-            SchemaName = "s",
-            Active = true,
-            Containers = new[] { new ContainerConfiguration { GoalWorkerCount = 4 } }
-        });
-        var configuration = system.ConfigurationApi().ReadConfigurations().Single();
-            
-        system.ConfigurationApi().DeleteConfiguration(configuration.Id);
+		var result = system.ConfigurationApi().ReadConfigurations().Single();
+		Assert.AreEqual(22, result.Id);
+		Assert.AreEqual("connection", result.ConnectionString);
+		Assert.AreEqual("SchemaName", result.SchemaName);
+		Assert.True(result.Active);
+		Assert.AreEqual(44, result.Containers.Single().GoalWorkerCount);
+	}
 
-        var result = system.ConfigurationApi().ReadConfigurations();
-        result.Should().Be.Empty();
-    }
+	[Test]
+	public void ShouldDelete()
+	{
+		var system = new SystemUnderTest();
+
+		system.ConfigurationApi().WriteConfiguration(new StoredConfiguration
+		{
+			Id = 3,
+			ConnectionString = "c",
+			SchemaName = "s",
+			Active = true,
+			Containers = new[] {new ContainerConfiguration {GoalWorkerCount = 4}}
+		});
+		var configuration = system.ConfigurationApi().ReadConfigurations().Single();
+
+		system.ConfigurationApi().DeleteConfiguration(configuration.Id);
+
+		var result = system.ConfigurationApi().ReadConfigurations();
+		result.Should().Be.Empty();
+	}
 }
